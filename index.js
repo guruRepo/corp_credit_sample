737bb22adf3ef8e88eac10fcfbc42ed96a7a5409
vinayg1 <vinayg1@nationwide.com>
2018-10-26 10:20:28 -0400

    added rest call

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ var app = express()
 app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function(request, response1) {
+   
+   request.get({url: "http://vg-inv.us-east-1.elasticbeanstalk.com:8080/dashboard"}, function(error, response2, body) { 
+          if (!error && response2.statusCode == 200) { 
+                response1.json(body); 
+          } 
+   }); 
 })
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
